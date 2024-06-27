344276c7a4c3cbd24d134dc1075d2794dc483e93
Zakir Rəhimli <zakir.rahimli@ateshgah.com>
2024-06-27 17:47:25 +0400

    IoC Container Logics Done with Unity Library.

--- Project.Solution/Project.ConsoleApp/AppCode/DI/MethodBased/SurveyService.cs
+++ Project.Solution/Project.ConsoleApp/AppCode/DI/MethodBased/SurveyService.cs
@@ -5,8 +5,25 @@ namespace Project.ConsoleApp.AppCode.DI.MethodBased
     // Define a class that depends on the email service
     public class SurveyService
     {
+        // 1) For DI
+
         // Method that requires the email service dependency to be passed
         public void SendNotification(IEmailService emailService, string recipient, string message)
+        {
+            // Logic to process and send notifications
+            emailService.SendEmail(recipient, "Survey", message);
+
+
+        }
+
+        // 2) For IoC
+        private IEmailService emailService;
+        public void Initialize(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+        public void SendNotificationIoC(string recipient, string message)
         {
             // Logic to process and send notifications
             emailService.SendEmail(recipient, "Survey", message);
--- Project.Solution/Project.ConsoleApp/Program.cs
+++ Project.Solution/Project.ConsoleApp/Program.cs
@@ -2,6 +2,8 @@
 using Project.ConsoleApp.AppCode.DI.ConstructorBased;
 using Project.ConsoleApp.AppCode.DI.MethodBased;
 using Project.ConsoleApp.AppCode.DI.PropertyBased;
+using Unity;
+using Unity.Injection;
 
 // Constants 
 const string RECIPIENT = "Zakir Rahimli";
@@ -9,17 +11,51 @@ const string RECIPIENT = "Zakir Rahimli";
 // Commons
 EmailService emailService = new();
 
+// I. Section - Dependency Injection (DI) in C#
+
 // 1) Constructor Injection
+Console.WriteLine("Constructor Injection:");
+
 InformationService ınformationService = new(emailService);
 ınformationService.SendNotification(RECIPIENT, "We hired you!");
 
 // 2) Property Injection
+Console.WriteLine("Property Injection:");
+
 WarningService warningService = new();
 warningService.EmailService = emailService;
 warningService.SendNotification(RECIPIENT, "2 days left until the project deadline!");
 
 // 3) Method Injection
+Console.WriteLine("Method Injection:");
+
 SurveyService surveyService = new();
 surveyService.SendNotification(emailService, RECIPIENT, "Are you satisfied with the service?");
+
+// II. Inversion of Control (IoC) Principle (Using 'Unity' Library)
+// Setting up Unity IoC container and registering dependencies
+UnityContainer unityContainer = new();
+unityContainer.RegisterType<IEmailService, EmailService>();
+
+// 1) IoC Container - Constrcutor Injection
+Console.WriteLine("IoC Container - Constructor Injection:");
+
+// Resolving and using the NotificationService with injected dependency
+InformationService iocInformationService = unityContainer.Resolve<InformationService>();
+iocInformationService.SendNotification(RECIPIENT, "Hello, this is a information!");
+
+// 2) IoC Container - Property Injection
+Console.WriteLine("IoC Container - Property Injection:");
+
+unityContainer.RegisterType<WarningService>(new InjectionProperty("EmailService"));
+WarningService iocWarningService = unityContainer.Resolve<WarningService>();
+iocWarningService.SendNotification(RECIPIENT, "Hello, this is a warning!");
+
+// 3) IoC Container - Method Injection
+Console.WriteLine("IoC Container - Method Injection:");
+
+unityContainer.RegisterType<SurveyService>(new InjectionMethod("Initialize", new ResolvedParameter<IEmailService>()));
+SurveyService iocSurveyService = unityContainer.Resolve<SurveyService>();
+iocSurveyService.SendNotificationIoC(RECIPIENT, "Hello, this is an survey!");
 
 Console.ReadKey();
